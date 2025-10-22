92902c495fbb2857ec35fbbc4ddbee205a9462d7
Yogthos <yogthos@gmail.com>
2025-10-22 10:07:05 -0400

    remove unused vars

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Set environment variables for libffi
-export LDFLAGS="-L/opt/homebrew/opt/libffi/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/libffi/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/libffi/lib/pkgconfig"
-
 source venv/bin/activate && python3 build_app.py
